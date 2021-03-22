9d5fde93ba565bcb0cdd3c2cf06ed2b77ef811d4
Francesco Degrassi <francesco.degrassi@optionfactory.net>
2021-03-22 12:01:35 +0100

    :fix: bumped image version

--- src/image.rs
+++ src/image.rs
@@ -1,6 +1,6 @@
 extern crate image as image_ext;
 
-use self::image_ext::{imageops, FilterType, GenericImage, ImageBuffer, Pixel};
+use self::image_ext::{imageops, GenericImage, ImageBuffer, Pixel};
 use super::Image;
 use super::ResizableImage;
 use super::RGB;
@@ -12,11 +12,11 @@ where
     P: Pixel<Subpixel = u8> + 'static,
 {
     fn width(&self) -> u32 {
-        GenericImage::width(self)
+        self.width()
     }
 
     fn height(&self) -> u32 {
-        GenericImage::height(self)
+        self.height()
     }
 
     fn get(&self, x: u32, y: u32) -> RGB {
@@ -35,6 +35,6 @@ where
     P: Pixel<Subpixel = u8> + 'static,
 {
     fn resize(&self, width: u32, height: u32) -> ImageBuffer<P, std::vec::Vec<u8>> {
-        imageops::resize(self, width, height, FilterType::Lanczos3)
+        imageops::resize(self, width, height, imageops::FilterType::Lanczos3)
     }
 }
